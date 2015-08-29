cf9ed1a3be809e21fb122b49bdb5694969cabc47
Aaron Kaka <aaron.kaka@pearson.com>
2015-08-29 03:21:35 -0600

    Achieve cross-browser compatibility and remove confusion. Only react_components is transpiled, linted, and hot reloaded, so eventing.js was left in the cold for browser support of 'let', which is now removed from that file. Additionally, now it is demonstrated that a ES5 app can use a transpiled ES6 component.

--- scripts/eventing.js
+++ scripts/eventing.js
@@ -2,7 +2,7 @@
 
 function init() {
 
-  let eventedElementId = 'eventedElement',
+  var eventedElementId = 'eventedElement',
       eventedElement = document.getElementById(eventedElementId),
       foucTarget = document.getElementsByClassName('no-fouc')[0];
 
@@ -17,50 +17,50 @@ function init() {
 
   // Data in detail objects could come from a service endpoint
 
-  let initComponent1 = new CustomEvent('initCard', {
+  eventedElement.dispatchEvent(new CustomEvent('initCard', {
       detail: {
-          eventedElem: eventedElementId,
-          targetElem: "div1",
-          username: "aaron.kaka",
-          bio: "This is Aaron's bio.",
-          avatar: "images/avatar.png"
+        eventedElem: eventedElementId,
+        targetElem: "div1",
+        username: "aaron.kaka",
+        bio: "This is Aaron's bio.",
+        avatar: "images/avatar.png"
       }
-  });
-  eventedElement.dispatchEvent(initComponent1);
+    })
+  );
 
-  let initComponent2 = new CustomEvent('initCard', {
+  eventedElement.dispatchEvent(new CustomEvent('initCard', {
       detail: {
-          eventedElem: eventedElementId,
-          targetElem: "div2",
-          username: "joe.schmo",
-          bio: "This is Joe's bio.",
-          avatar: "images/wired.jpg"
+        eventedElem: eventedElementId,
+        targetElem: "div2",
+        username: "joe.schmo",
+        bio: "This is Joe's bio.",
+        avatar: "images/wired.jpg"
       }
-  });
-  eventedElement.dispatchEvent(initComponent2);
+    })
+  );
 
-  let initComponent3 = new CustomEvent('initCard', {
+  eventedElement.dispatchEvent(new CustomEvent('initCard', {
       detail: {
-          eventedElem: eventedElementId,
-          targetElem: "div3",
-          username: "suzie.q",
-          bio: "This is Suzie's bio.",
-          avatar: "images/suzieq.jpg"
+        eventedElem: eventedElementId,
+        targetElem: "div3",
+        username: "suzie.q",
+        bio: "This is Suzie's bio.",
+        avatar: "images/suzieq.jpg"
       }
-  });
-  eventedElement.dispatchEvent(initComponent3);
+    })
+  );
 
   // Outside the evented group
-  let initComponent4 = new CustomEvent('initCard', {
+  document.getElementById('outsider').dispatchEvent(new CustomEvent('initCard', {
       detail: {
-          // NO eventedElem
-          targetElem: "div4",
-          username: "outsider.card",
-          bio: "This card does not belong to the evented group of cards."
-          // NO avatar
+        // NO eventedElem
+        targetElem: "div4",
+        username: "outsider.card",
+        bio: "This card does not belong to the evented group of cards."
+        // NO avatar
       }
-  });
-  document.getElementById('outsider').dispatchEvent(initComponent4);
+    })
+  );
 
 }
 
