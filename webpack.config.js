cd44c700b6f80a8aacc640b83c1eb66960ed100e
Aaron Kaka <aaron.kaka@pearson.com>
2015-09-20 04:07:22 -0600

    Beef up npm scripts: add coverage for Jest testing, add debug eslint, add build for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,13 @@ module.exports = {
       {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
     ],
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/},
-      {test: /\.woff$/, loader: 'url?limit=100000'},
-      {test: /\.eot$/, loader: 'url?limit=100000'},
-      {test: /\.svg$/, loader: 'url?limit=100000'},
-      {test: /\.ttf$/, loader: 'url?limit=100000'}
+      {test: /\.js$/, loader: 'jsx-loader', exclude: /node_modules/},
+      {test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/},
+      {test: /\.js$/, loader: "babel-loader", exclude: /node_modules/},
+      {test: /\.woff$/, loader: 'url?limit=100000', exclude: /node_modules/},
+      {test: /\.eot$/, loader: 'url?limit=100000', exclude: /node_modules/},
+      {test: /\.svg$/, loader: 'url?limit=100000', exclude: /node_modules/},
+      {test: /\.ttf$/, loader: 'url?limit=100000', exclude: /node_modules/}
     ]
   }
 };
