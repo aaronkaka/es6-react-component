8e395cf1641a4c60ae467ab8b60594839204cb40
Aaron Kaka <aaron.kaka@pearson.com>
2016-02-18 20:14:41 -0700

    refactor(release): Exit failure if the release script is executed in other than the master branch.

--- scripts/release.js
+++ scripts/release.js
@@ -12,6 +12,7 @@ const stdin = readline.createInterface({
 function syncRemote(branchName, nextVersion) {
   exec(`git push origin ${branchName}`);
   exec(`git push --tags`);
+  console.log(`Synced local '${branchName}' with remote as ${nextVersion}`);
 }
 
 function exitFailure(message) {
@@ -19,38 +20,36 @@ function exitFailure(message) {
   process.exit(1);
 }
 
-if (branchName === 'master') {
-
-  console.log('On master branch, check!');
-
-  stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
-
-    if (!semver.valid(nextVersion)) {
-      exitFailure(`Version '${nextVersion}' is not valid: it must be a valid semantic version. See http://semver.org/.`);
-    }
-
-    if (!semver.gt(nextVersion, currentVersion)) {
-      exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
-    }
-
-    if (nextVersion.startsWith('v')) {
-      nextVersion = nextVersion.slice(1);
-    }
-
-    // Make sure unit tests pass before continuing!
-    exec('npm test');
-
-    // Ensure the build is generated
-    exec('npm run build');
-
-    // Locally commit the version update in package.json (also, if present, npm-shrinkwrap.json) and create tag
-    exec(`npm version ${nextVersion}`);
-
-    syncRemote(branchName, nextVersion);
-
-    exec('npm publish');
-
-    stdin.close();
-  });
-
+if (branchName !== 'master') {
+  exitFailure('You must be on the master branch to run this script.');
 }
+
+stdin.question(`Next version (current is ${currentVersion})? `, (nextVersion) => {
+
+  if (!semver.valid(nextVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: it must be a valid semantic version. See http://semver.org/.`);
+  }
+
+  if (!semver.gt(nextVersion, currentVersion)) {
+    exitFailure(`Version '${nextVersion}' is not valid: it must be greater than the current version.`);
+  }
+
+  if (nextVersion.startsWith('v')) {
+    nextVersion = nextVersion.slice(1);
+  }
+
+  // Make sure unit tests pass before continuing!
+  exec('npm test');
+
+  // Ensure the build is generated
+  exec('npm run build');
+
+  // Locally commit the version update in package.json (also, if present, npm-shrinkwrap.json) and create tag
+  exec(`npm version ${nextVersion}`);
+
+  syncRemote(branchName, nextVersion);
+
+  exec('npm publish');
+
+  stdin.close();
+});
