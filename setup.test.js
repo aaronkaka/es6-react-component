ff930b2c5d6bb638ff00920dd20998825b54e9b4
Kaka, Aaron A <Aaron.Kaka@pearson.com>
2018-02-13 09:00:48 -0500

    chore: Add setup.test.js to scope of lint-all npm script, and ignore it for testing.

--- setup.test.js
+++ setup.test.js
@@ -3,7 +3,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-it('proves the test setup file is processed', () => {
-  expect('test setup').toEqual('test setup');
-});
